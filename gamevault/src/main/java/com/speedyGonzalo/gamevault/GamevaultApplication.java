183e25329a0650f613e0fd64728f64dafbbbec55
Samin Ahsan <turzo68@gmail.com>
2022-08-16 18:47:52 +0600

    changed backend from express to spring boot

---
+++ gamevault/src/main/java/com/speedyGonzalo/gamevault/GamevaultApplication.java
@@ -0,0 +1,13 @@
+package com.speedyGonzalo.gamevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamevaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamevaultApplication.class, args);
+	}
+
+}
---
+++ gamevault/src/test/java/com/speedyGonzalo/gamevault/GamevaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speedyGonzalo.gamevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamevaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
